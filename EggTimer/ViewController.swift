c33079e6f5a9e3cea309438a2f0fce0411663de9
user161760 <user161760@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-12-31 14:23:24 -0500

    Timer implementado

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggTimes : [String : Int] = ["Soft":1,"Medium":7,"Hard":12]
+    var timer = Timer()
+    var seconds = 60
+    var isTimerRunning = false
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        let result = eggTimes[hardness]!
+        seconds = 60*result
+        if !isTimerRunning {
+            runTimer()
+        }
+    }
+    
+    func runTimer() {
+    timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+   
+    isTimerRunning = true
+    }
+    
+    func stopTimer(){
+        timer.invalidate()
+        isTimerRunning = false
+    }
+    
+    @objc func updateTimer(){
+        if seconds>=0 {
+            print(seconds)
+            seconds -= 1
+        }
+        else{
+            stopTimer()
+            seconds = 60
+        }
+    }
 }
